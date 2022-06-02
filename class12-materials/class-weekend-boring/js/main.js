053162cc74b8fe1a999cc33bb897391274d31265
acopperlily <acopperlily@yahoo.com>
2022-06-02 05:24:31 -0400

    Finish background picker, calculator, class weekend

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -2,9 +2,24 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
-  //Conditionals go here
+  let result;
 
+  if (day === 'tuesday' || day === 'thursday') {
+    result = 'CLASS DAY!';
+  } else if (day === 'saturday' || day === 'sunday') {
+    result = 'Weekend!';
+    if (day === 'sunday') {
+      result += ' And we have OFFICE HOURS!';
+    } else {
+      result += ' Happy Caturday!';
+    }
+  } else {
+    result = 'BORRRRINNNNNGGGGGG!!!';
+  }
+
+  const display = document.querySelector('#placeToSee');
+  display.textContent = result;
 
 }
