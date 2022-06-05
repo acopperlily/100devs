3518119f2ff4121438e5f26ad4aac8dfb47d4479
acopperlily <acopperlily@yahoo.com>
2022-06-05 05:21:23 -0400

    Create Three Simple Functions

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,33 @@
 //create a function that adds two numbers and alerts the sum
 
-//create a function that multiplys three numbers and console logs the product
+function addTwoNumbers(num1, num2) {
+  const result = num1 + num2;
+  alert(result);
+  return result;
+}
 
-//create a function that divides two numbers and returns the ???
+//create a function that multiplys three numbers and console logs the product
+function multiplyThreeNumbers(num1, num2, num3) {
+  const result = num1 * num2 * num3;
+  console.log(result);
+  return result;
+}
+
+//create a function that divides two numbers and returns the ??? QUOTIENT LOLOLOL
+
+function divideTwoNumbers(num1, num2) {
+  let result;
+  if (num2 === 0) {
+    result = "You've ruined everything!";
+  } else {
+    result = num1 / num2;
+  }
+  return result;
+}
+
+// Grab elements and display values on page
+document.getElementById('in-alert').textContent = addTwoNumbers(42, 27);
+
+document.getElementById('in-console').textContent = multiplyThreeNumbers(4, 5, 21);
+
+document.getElementById('returned').textContent = divideTwoNumbers(420, 10);
