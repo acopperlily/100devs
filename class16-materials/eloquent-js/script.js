8f88ed63964e7bab506b96ccd84a9d8f868e4dc8
acopperlily <acopperlily@yahoo.com>
2022-06-09 09:23:13 -0400

    Finish Eloquent JS Chapters 1-3

--- class16-materials/eloquent-js/script.js
+++ class16-materials/eloquent-js/script.js
@@ -35,6 +35,55 @@ for (let i = 1; i <= 100; i++) {
 # # # # 
 When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.*/
 
-const chessboard = ' # # # #\n# # # # '
 let size = 8;
-for (let i = 0; i < size / 2; i++) console.log(chessboard);
+let board = '';
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    if ((j+i) % 2 === 0) board += ' ';
+    else board += '#';
+  }
+  board += '\n';
+}
+console.log(board);
+
+/* Write a function min that takes two arguments and returns their minimums */
+
+const min = (num1, num2) => num1 < num2 ? num1 : num2;
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+/* Define a recursive function isEven. The function should accept a single parameter (a positive, whole number) and return a Boolean. */
+
+function isEven(n) {
+  n = Math.abs(n);
+  return n === 0 ? true : n === 1 ? false : isEven(n - 2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+/* Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase "B" characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted. Rewrite countBs to make use of this new function. */
+
+function countBs(str) {
+  let count = 0;
+  for (let i = 0, n = str.length; i < n; i++) {
+    if (str[i] === 'B') count++;
+  }
+  return count;
+}
+
+console.log(countBs('BBC'));
+
+const countChar = (str, char) => {
+  let count = 0;
+  for (let i = 0, n = str.length; i < n; i++) {
+    if (str[i] === char) count++;
+  }
+  return count;
+}
+
+console.log(countChar('kakkerlak', 'k'));
