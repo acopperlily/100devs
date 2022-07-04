57e0a5cea071ef4b17f866de5ac1c111dd424e8b
acopperlily <acopperlily@yahoo.com>
2022-07-04 05:10:11 -0400

    Add carousel to cocktail app

--- class26-materials/cocktails/script.js
+++ class26-materials/cocktails/script.js
@@ -1,7 +1,23 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', getDrink);
+window.onload = getDrinks;
 
-function getDrink() {
+const btn = document.querySelector('button');
+btn.addEventListener('click', getDrinks);
+
+const prev = document.getElementById('prev');
+prev.addEventListener('click', e => {
+  scrollDrinks(-1);
+});
+
+const next = document.getElementById('next');
+next.addEventListener('click', e => {
+  scrollDrinks(1);
+});
+
+let count = 0;
+let drinkArray;
+
+function getDrinks(e) {
+  e.preventDefault();
   let drink = document.querySelector('input').value;
   // Sanitize input
   drink = drink.toLowerCase().trim();
@@ -14,15 +30,29 @@ function getDrink() {
     .then(res => res.json())
     .then(data => {
       console.log(data);
-      const drinkName = document.querySelector('h2');
-      drinkName.textContent = data.drinks[0].strDrink;
-      const img = document.querySelector('img');
-      img.src = data.drinks[0].strDrinkThumb;
-      const instructions = document.querySelector('h3');
-      instructions.textContent = data.drinks[0].strInstructions;
+      drinkArray = [];
+      data.drinks.forEach(drink => drinkArray.push(drink));
+      count = 0;
+      setDrink(count);
     })
     .catch(err => {
       console.log(`error ${err}`);
     });
 }
 
+function scrollDrinks(change) {
+  count += change;
+  if (count < 0) count = drinkArray.length - 1;
+  else if (count >= drinkArray.length) count = 0;
+  setDrink(count);
+}
+
+function setDrink(count) {
+  console.log(drinkArray[count]);
+  const drinkName = document.querySelector('h2');
+  drinkName.textContent = drinkArray[count].strDrink;
+  const img = document.querySelector('img');
+  img.src = drinkArray[count].strDrinkThumb;
+  const instructions = document.querySelector('p');
+  instructions.textContent = drinkArray[count].strInstructions;
+}
