6455017ef7aaad01bba410c5a7ae362426e8cf1d
acopperlily <acopperlily@yahoo.com>
2022-07-10 05:36:17 -0400

    Continue Eloquent JS Objects

---
+++ class29-materials/eloquent-js-secret-life-objects/script.js
@@ -0,0 +1,111 @@
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbit = new Rabbit('killer');
+let blackRabbit = new Rabbit('black');
+
+Rabbit.prototype.teeth = 'small';
+console.log(killerRabbit.teeth);
+
+killerRabbit.teeth = 'long, sharp, and bloody';
+console.log(killerRabbit.teeth);
+console.log(blackRabbit.teeth);
+console.log(Rabbit.prototype.teeth);
+
+let sym = Symbol('name');
+console.log(sym == Symbol('name'));
+
+Rabbit.prototype[sym] = 55;
+console.log(blackRabbit[sym]);
+console.log();
+
+let okIterator = 'OK'[Symbol.iterator]();
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next(), '\n');
+
+class Matrix {
+  constructor(width, height, element = (x, y) => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = element(x, y);
+      }
+    }
+  }
+
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next() {
+    if (this.y == this.matrix.height) return {done: true};
+
+    let value = {x: this.x,
+                y: this.y,
+                value: this.matrix.get(this.x, this.y)};
+    
+    this.x++;
+    if (this.x == this.matrix.width) {
+      this.x = 0;
+      this.y++;
+    }
+    return {value, done: false};
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x},${y}`);
+for (let {x, y, value} of matrix) {
+  console.log(x, y, value);
+}
+
+let varyingSize = {
+  get size() {
+    return Math.floor(Math.random() * 100);
+  }
+};
+console.log(varyingSize.size);
+console.log(varyingSize.size, '\n');
+
+class SymmetricMatrix extends Matrix {
+  constructor(size, element = (x, y) => undefined) {
+    super(size, size, (x, y) => {
+      if (x < y) return element(y, x);
+      else return element(x, y);
+    });
+  }
+
+  set(x, y, value) {
+    super.set(x, y, value);
+    if (x != y) {
+      super.set(y, x, value);
+    }
+  }
+}
+
+let evenMatrix = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
+console.log(evenMatrix.get(2, 3));
